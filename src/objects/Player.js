77ac96bc518f4790ac8154080784ef8658215ff8
Dima-macbook <dima@virtuman.com>
2019-01-06 23:13:35 +0300

    added new way for player graphic, added controls for player sprite

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -1,10 +1,10 @@
+import Phaser from 'phaser';
 import {GraphicUtil} from '../utils/graphic-util';
 
 class Player {
-
 	constructor(game){
 		this.game = game;
-		const x = this.game.width / 2;
+		const x = this.game.width / 2 - 10;
         const y = this.game.height / 5 * 4;
 		this.playerProps = {
             x,
@@ -21,11 +21,46 @@ class Player {
                 alpha: 0.5,
             },
         };
+        this.movementValue = 5;
 	}
 
 	spawn() {
-		return GraphicUtil.initCircleItem(this.playerProps, this.game);
-	}
+        const player = GraphicUtil.getCircleItem(this.playerProps, this.game);
+        const playerTexture = player.generateTexture();
+        this.playerSprite = this.game.add.sprite(this.playerProps.x, this.playerProps.y, playerTexture);
+        this.game.physics.arcade.enable(this.playerSprite);
+        this.playerSprite.body.collideWorldBounds = true;
+        this.playerSprite.body.bounce.set(0);
+		return this.playerSprite;
+    }
+    
+    addControls() {
+        const leftKey = this.game.input.keyboard.addKey(Phaser.KeyCode.LEFT);
+        const rightKey = this.game.input.keyboard.addKey(Phaser.KeyCode.RIGHT);
+
+        leftKey.onHoldContext = this;
+        leftKey.onHoldCallback = this.moveLeft;
+        rightKey.onHoldContext = this;
+        rightKey.onHoldCallback = this.moveRight;
+        leftKey.onUp.add(this.stopMovement, this);
+        rightKey.onUp.add(this.stopMovement, this);
+    }
+
+    moveLeft() {
+        this.move(-this.movementValue);
+    }
+
+    moveRight() {
+        this.move(this.movementValue);
+    }
+
+    move(velocityValue) {
+        this.playerSprite.body.velocity.x += velocityValue;
+    }
+
+    stopMovement() {
+        this.playerSprite.body.velocity.x = 0;
+    }
 
 }
 
--- src/utils/graphic-util.js
+++ src/utils/graphic-util.js
@@ -3,20 +3,6 @@
 import Phaser from 'phaser';
 
 class GraphicUtil {
-    static initCircleItem(itemProps, game) {
-        const lineStyle = itemProps.lineStyle;
-        const circle = itemProps.circle;
-        const graphicObject = game.add.graphics(0, 0);
-        graphicObject.beginFill(itemProps.color);
-        graphicObject.lineStyle(lineStyle.width, lineStyle.color, lineStyle.alpha);
-        graphicObject.drawCircle(circle.x, circle.y, circle.diameter);
-
-        const item = game.add.sprite(itemProps.x, itemProps.y, null);
-        item.addChild(graphicObject);
-
-        return item;
-    }
-
     static getCircleItem(itemProps, game) {
         const lineStyle = itemProps.lineStyle;
         const circle = itemProps.circle;
